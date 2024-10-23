8f3b5621c56e9a8a693fe6514863e4f00ad5f64f
Christian Monch <christian.moench@web.de>
2024-10-23 08:53:34 +0200

    test: skip windows tests that would fail

--- datalad_remake/annexremotes/tests/test_remake_remote.py
+++ datalad_remake/annexremotes/tests/test_remake_remote.py
@@ -2,6 +2,7 @@ import subprocess
 from queue import Queue
 
 from annexremote import Master
+from datalad_next.tests import skip_if_on_windows
 
 from datalad_remake.commands.tests.create_datasets import create_ds_hierarchy
 
@@ -60,6 +61,7 @@ class MockedInput:
         self.input.put(value)
 
 
+@skip_if_on_windows
 def test_compute_remote_main(tmp_path, monkeypatch):
     dataset = create_ds_hierarchy(tmp_path, 'ds1', 0)[0][2]
     monkeypatch.chdir(dataset.path)
